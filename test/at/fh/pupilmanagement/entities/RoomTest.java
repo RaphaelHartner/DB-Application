05e3b73e684695970b22c5ae1b931511f85c02a0
RaphaelHartner <raphael.hartner@gmx.at>
2015-11-25 21:34:49 +0100

    configure test

--- test/at/fh/pupilmanagement/entities/RoomTest.java
+++ test/at/fh/pupilmanagement/entities/RoomTest.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import org.eclipse.persistence.queries.ReadAllQuery;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -51,11 +52,12 @@ public class RoomTest {
 
 		room.setMaxPupils(changedNumber);
 		room.setName(room_name + changedNumber);
-		room.setPosition(room_name + changedNumber);
+		room.setPosition(room_position + changedNumber);
 
 		entitymanager.getTransaction().commit();
 
 		room = entitymanager.find(Room.class, ID);
+		Assert.assertNotNull(room);
 		Assert.assertEquals(room_name + changedNumber, room.getName());
 		Assert.assertEquals(room_position + changedNumber, room.getPosition());
 		Assert.assertEquals(changedNumber, changedNumber);
@@ -64,4 +66,25 @@ public class RoomTest {
 		emfactory.close();
 	}
 
+	@Test
+	public void testAdd() {
+		EntityManagerFactory emfactory = Persistence
+				.createEntityManagerFactory("PupilManagement");
+		EntityManager entitymanager = emfactory.createEntityManager();
+		entitymanager.getTransaction().begin();
+
+		Room newRoom = new Room();
+		newRoom.setMaxPupils(25);
+		newRoom.setName("TestName");
+		newRoom.setPosition("TestPosition");
+
+		entitymanager.persist(newRoom);
+		entitymanager.getTransaction().commit();
+		
+		
+		
+		entitymanager.close();
+		emfactory.close();
+
+	}
 }
